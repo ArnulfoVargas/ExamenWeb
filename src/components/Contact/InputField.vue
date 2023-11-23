7aefefc6d5cc440110bd4cf3e4b7e3a8403fbc51
ArnulfoVargas <vargas.mejia.arnulfo04@gmail.com>
2023-11-23 12:59:09 -0600

    contact skeleton

---
+++ src/components/Contact/InputField.vue
@@ -0,0 +1,68 @@
+<script setup>
+    defineProps({
+        label: String,
+        placeholder: String,
+    })
+</script>
+
+<template>
+    <div class="container">
+        <div>
+            <p>{{ label }}</p>
+        </div>
+
+        <input type="text" :placeholder="placeholder">
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        display: grid;
+        height: 3rem;
+        grid-template-columns: 1fr 2fr;
+        border: var(--thin-border);
+        margin-top: .5rem;
+        box-shadow: var(--box-shadow);
+    }
+
+    .container > div{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: var(--color-red);
+    }
+    
+    input{
+        border-radius: 0;
+        border: none;
+        border-left: var(--thin-border);
+        color: var(--color-gray);
+        font-family: var(--titles-font);
+        font-size: 1.5rem;
+        margin: 0;
+        padding-left: 10px;
+    }
+
+    input:focus{
+        outline: none;
+    }
+
+    input::placeholder{
+        font-family: var(--titles-font);
+        font-size: 1.5rem;
+        margin: 0;
+        color: rgba(57 52 52/ 0.5);
+    }
+
+    .top-none{
+        border-top: none;
+    }
+
+    p{
+        color: var(--color-white);
+        font-family: var(--main-font);
+        font-size: 1.5rem;
+        margin: 0;
+        font-weight: bold;
+    }
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,51 @@
 <script setup>
-    
+    import CartoonSubtitle from '../components/CartoonSubtitle.vue';
+    import InputField from '../components/Contact/InputField.vue';
+
 </script>
 
 <template>
     <div>
-        <h1>Contact</h1>
+        <CartoonSubtitle>Contact Me!</CartoonSubtitle>
+
+        <div class="form-container">
+            <InputField :label="'Name'" :placeholder="'Your name'" name="name"/>
+            <InputField :label="'Email'" :placeholder="'Your email'" name="email"/>
+
+            <textarea name="message" cols="30" rows="10" placeholder="Your message here" value="">
+
+            </textarea>
+        </div>
     </div>
 </template>
 
 <style scoped>
-    
+    .form-container{
+        margin: auto;
+        max-width: 500px;
+    }
+
+    textarea{
+        margin-top: .5rem;
+        margin-bottom: 1rem;
+        width: calc(100% - 20px);
+        resize: vertical;
+        border: var(--thin-border);
+        box-shadow: var(--box-shadow);
+        color: var(--color-gray);
+        font-family: var(--titles-font);
+        font-size: 1.25rem;
+        padding: 10px;
+    }
+
+    textarea:focus{
+        outline: none;
+    }
+
+    textarea::placeholder{
+        font-family: var(--titles-font);
+        font-size: 1.25rem;
+        margin: 0;
+        color: rgba(57 52 52/ 0.5);
+    }
 </style>
